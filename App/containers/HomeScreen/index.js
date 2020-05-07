77ebc995b43b21dc9699ddcf3aae0d3f40bc429c
Mohamed Mamdouh <meldakhakhni.gts@emaar.ae>
2020-05-07 17:04:31 +0400

    implement HomeScreen

--- App/containers/HomeScreen/index.js
+++ App/containers/HomeScreen/index.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect} from 'react';
+import {View, FlatList, ActivityIndicator, Alert} from 'react-native';
+import HomeActions from '../../redux/HomeRedux';
+import HolidaySection from '../../components/HolidaySection';
+import styles from './styles';
+import {useDispatch, useSelector} from 'react-redux';
+
+const countryCode = 'polish';
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+  const {holidaysRequest} = HomeActions;
+  const isFetching = useSelector((state) => state.home.fetching);
+  const data = useSelector((state) => state.home.data);
+
+  useEffect(() => {
+    dispatch(holidaysRequest(countryCode));
+  }, [dispatch, holidaysRequest]);
+
   return (
-    <View>
-      <View />
+    <View style={styles.container}>
+      <View style={styles.listConainer}>
+        {isFetching ? (
+          <ActivityIndicator />
+        ) : (
+          <FlatList
+            data={data}
+            renderItem={({item}) => <HolidaySection data={item} />}
+            keyExtractor={(item) => item.id}
+          />
+        )}
+      </View>
     </View>
   );
 };
